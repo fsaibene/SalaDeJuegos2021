e81a34693a7881828a0df14281f46e77a1f33469
Fernando Saibene <fsaibene@axoft.com>
2021-03-20 19:39:55 -0300

    primer commit con heroku coonfig ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AboutMeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
